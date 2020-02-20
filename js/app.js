251d92c5f486f2d1809c69cb89c307092433d34d
cody li <licody2000@gmail.com>
2020-02-19 22:46:08 -0800

    inital version

---
+++ js/app.js
@@ -0,0 +1,8 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
+particlesJS.load('particles-js2', 'particles2.json', function() {
+  console.log('particles2.js loaded - callback');
+});
+
